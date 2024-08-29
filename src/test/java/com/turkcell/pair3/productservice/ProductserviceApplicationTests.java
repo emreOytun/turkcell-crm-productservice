774f28e99abfd81107f43c020b79033297582b7e
emreOytun <emreoytun58@gmail.com>
2024-08-29 22:17:06 +0300

    config class for test class added
    (cherry picked from commit 05806304ea938e73cffdabd917b974c90030112b)

--- src/test/java/com/turkcell/pair3/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/turkcell/pair3/productservice/ProductserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.turkcell.pair3.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductserviceApplication.class)
 class ProductserviceApplicationTests {
 
     @Test
